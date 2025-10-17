607ce51b4321ab7d2a119768b084c01e2daa85d7
DanilishinArtem <danilishin-artem@mail.ru>
2025-10-17 14:13:24 +0300

    .

--- Python/Practic/Testing_Debugging/test.py
+++
@@ -1,9 +0,0 @@
-import logging
-logging.basicConfig(level=logging.DEBUG)
-
-if __name__ == "__main__":
-    logging.debug('[DBUG]')
-    logging.info('[INFO]')
-    logging.warn('[WARNING]')
-    logging.error('[ERROR]')
-    logging.critical('[CRITICAL]')
