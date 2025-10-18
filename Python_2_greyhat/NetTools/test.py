e47f8e5a19704a09e27c549d0e015f5ea89f0eb9
adanilishin <danilishin-artem@mail.ru>
2025-10-19 00:20:24 +0300

    added netcat

--- Python_2_greyhat/NetTools/test.py
+++ Python_2_greyhat/NetTools/test.py
@@ -1,26 +1,18 @@
 import socket
-import threading
 
-ip = '0.0.0.0'
-port = 9998
-
-def handle_client(client_socket):
-    with client_socket as sock:
-        request = sock.recv(1024)
-        print(f'[*] Received: {request.decode('utf-8')}')
-        sock.send(b'ACK')
-
-def main():
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((ip, port))
-    server.listen(5)
-    print(f'[*] Listening on {ip}:{port}')
-
-    while True:
-        client, address = server.accept()
-        print(f'[*] Accepted connection from {address[0]}:{address[1]}')
-        client_handler = threading.Thread(target=handle_client, args=(client,))
-        client_handler.start()
+target_host = '172.31.239.69'
+target_port = 9995
 
 if __name__ == "__main__":
-    main()
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((target_host, target_port))
+    client.send(b'GET / HTTP/1.1\r\nHOST: google.com\r\n\r\n')
+    response = client.recv(4096)
+    print(response.decode())
+    client.close()
+
+
+string = '''
+if __name__ == "__main__":
+    print('[INFO] Hello world')
+'''
